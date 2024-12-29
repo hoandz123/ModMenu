1608ae9a66a698ead747b65594fab2ca397e58af
hoandz123 <thanhhoan14082004@gmail.com>
2024-12-29 14:36:00 +0700

    Update assets and Menu.java

--- app/src/main/jni/Main.cpp
+++ app/src/main/jni/Main.cpp
@@ -1,3 +1,5 @@
+#define targetLibName OBFUSCATE("libil2cpp.so")
+
 #include <list>
 #include <vector>
 #include <string.h>
@@ -14,168 +16,33 @@
 #include "Includes/Utils.h"
 #include "KittyMemory/MemoryPatch.h"
 #include "Menu/Setup.h"
-
-//Target lib here
-#define targetLibName OBFUSCATE("libFileA.so")
-
 #include "Includes/Macros.h"
 
-bool feature1, feature2, featureHookToggle, Health;
-int sliderValue = 1, level = 0;
-void *instanceBtn;
-
-// Hooking examples. Assuming you know how to write hook
-void (*AddMoneyExample)(void *instance, int amount);
-
-bool (*old_get_BoolExample)(void *instance);
-bool get_BoolExample(void *instance) {
-    if (instance != NULL && featureHookToggle) {
-        return true;
-    }
-    return old_get_BoolExample(instance);
-}
-
-float (*old_get_FloatExample)(void *instance);
-float get_FloatExample(void *instance) {
-    if (instance != NULL && sliderValue > 1) {
-        return (float) sliderValue;
-    }
-    return old_get_FloatExample(instance);
-}
-
-int (*old_Level)(void *instance);
-int Level(void *instance) {
-    if (instance != NULL && level) {
-        return (int) level;
-    }
-    return old_Level(instance);
-}
-
-void (*old_FunctionExample)(void *instance);
-void FunctionExample(void *instance) {
-    instanceBtn = instance;
-    if (instance != NULL) {
-        if (Health) {
-            *(int *) ((uint64_t) instance + 0x48) = 999;
-        }
-    }
-    return old_FunctionExample(instance);
-}
-
-// we will run our hacks in a new thread so our while loop doesn't block process main thread
 void *hack_thread(void *) {
-    LOGI(OBFUSCATE("pthread created"));
-
-    //Check if target lib is loaded
     do {
         sleep(1);
     } while (!isLibraryLoaded(targetLibName));
 
-    //Anti-lib rename
-    /*
-    do {
-        sleep(1);
-    } while (!isLibraryLoaded("libYOURNAME.so"));*/
-
-    LOGI(OBFUSCATE("%s has been loaded"), (const char *) targetLibName);
-
-#if defined(__aarch64__) //To compile this code for arm64 lib only. Do not worry about greyed out highlighting code, it still works
-    // Hook example. Comment out if you don't use hook
-    // Strings in macros are automatically obfuscated. No need to obfuscate!
-    HOOK("str", FunctionExample, old_FunctionExample);
-    HOOK_LIB("libFileB.so", "0x123456", FunctionExample, old_FunctionExample);
-    HOOK_NO_ORIG("0x123456", FunctionExample);
-    HOOK_LIB_NO_ORIG("libFileC.so", "0x123456", FunctionExample);
-    HOOKSYM("__SymbolNameExample", FunctionExample, old_FunctionExample);
-    HOOKSYM_LIB("libFileB.so", "__SymbolNameExample", FunctionExample, old_FunctionExample);
-    HOOKSYM_NO_ORIG("__SymbolNameExample", FunctionExample);
-    HOOKSYM_LIB_NO_ORIG("libFileB.so", "__SymbolNameExample", FunctionExample);
-
-    // Patching offsets directly. Strings are automatically obfuscated too!
-    PATCH("0x20D3A8", "00 00 A0 E3 1E FF 2F E1");
-    PATCH_LIB("libFileB.so", "0x20D3A8", "00 00 A0 E3 1E FF 2F E1");
-
-    AddMoneyExample = (void(*)(void *,int))getAbsoluteAddress(targetLibName, 0x123456);
-
-#else //To compile this code for armv7 lib only.
-
-    // Hook example. Comment out if you don't use hook
-    // Strings in macros are automatically obfuscated. No need to obfuscate!
-    HOOK("str", FunctionExample, old_FunctionExample);
-    HOOK_LIB("libFileB.so", "0x123456", FunctionExample, old_FunctionExample);
-    HOOK_NO_ORIG("0x123456", FunctionExample);
-    HOOK_LIB_NO_ORIG("libFileC.so", "0x123456", FunctionExample);
-    HOOKSYM("__SymbolNameExample", FunctionExample, old_FunctionExample);
-    HOOKSYM_LIB("libFileB.so", "__SymbolNameExample", FunctionExample, old_FunctionExample);
-    HOOKSYM_NO_ORIG("__SymbolNameExample", FunctionExample);
-    HOOKSYM_LIB_NO_ORIG("libFileB.so", "__SymbolNameExample", FunctionExample);
-
-    // Patching offsets directly. Strings are automatically obfuscated too!
-    PATCH("0x20D3A8", "00 00 A0 E3 1E FF 2F E1");
-    PATCH_LIB("libFileB.so", "0x20D3A8", "00 00 A0 E3 1E FF 2F E1");
-
-    //Restore changes to original
-    RESTORE("0x20D3A8");
-    RESTORE_LIB("libFileB.so", "0x20D3A8");
-
-    AddMoneyExample = (void (*)(void *, int)) getAbsoluteAddress(targetLibName, 0x123456);
-
-    LOGI(OBFUSCATE("Done"));
-#endif
-
-    //Anti-leech
-    /*if (!iconValid || !initValid || !settingsValid) {
-        //Bad function to make it crash
-        sleep(5);
-        int *p = 0;
-        *p = 0;
-    }*/
-
     return NULL;
 }
 
-// Do not change or translate the first text unless you know what you are doing
-// Assigning feature numbers is optional. Without it, it will automatically count for you, starting from 0
-// Assigned feature numbers can be like any numbers 1,3,200,10... instead in order 0,1,2,3,4,5...
-// ButtonLink, Category, RichTextView and RichWebView is not counted. They can't have feature number assigned
-// Toggle, ButtonOnOff and Checkbox can be switched on by default, if you add True_. Example: CheckBox_True_The Check Box
-// To learn HTML, go to this page: https://www.w3schools.com/
 struct Feature {
     int number = -1;
     std::string category;
     std::string flag;
     std::string description;
 };
-std::string ConvertFeatureToString(const Feature &feature) {
-    std::ostringstream oss;
-
-    if (feature.number != -1) {
-        oss << feature.number << "_";
-    }
-    oss << feature.category;
-    if (!feature.flag.empty()) {
-        oss << "_" << feature.flag;
-    }
-    if (!feature.description.empty()) {
-        oss << "_" << feature.description;
-    }
-
-    return oss.str();
-}
-
 jobjectArray GetFeatureList(JNIEnv *env, jobject context) {
-    jobjectArray ret;
-
     Feature features[] = {
             //id    view        TAB     name
             {100,   "Category",  "A",   "Thông Báo"},
-            {101,   "Toggle",    "A",   "Thông "},
-            {102,   "Toggle",    "B",   "The toggle 2"},
+            {101,   "Toggle",    "A",   "Chức Năng 1"},
+            {102,   "Toggle",    "B",   "Chức Năng 2"},
     };
 
 
     int Total_Feature = (sizeof features / sizeof features[0]);
-    ret = (jobjectArray)
+    jobjectArray ret = (jobjectArray)
             env->NewObjectArray(Total_Feature, env->FindClass(OBFUSCATE("java/lang/String")),
                                 env->NewStringUTF(""));
 
@@ -204,82 +71,12 @@ void Changes(JNIEnv *env, jclass clazz, jobject obj,
 
     switch (featNum) {
         case 0:
-            // MOV R0, #0x0 = 00 00 A0 E3
-            // BX LR = 1E FF 2F E1
-            PATCH_LIB_SWITCH("libil2cpp.so", "0x100000", "00 00 A0 E3 1E FF 2F E1", boolean);
-            break;
-        case 100:
-            //Reminder that the strings are auto obfuscated
-            //Switchable patch
-            PATCH_SWITCH("0x400000", "00 00 A0 E3 1E FF 2F E1", boolean);
-            PATCH_LIB_SWITCH("libil2cpp.so", "0x200000", "00 00 A0 E3 1E FF 2F E1", boolean);
-            PATCH_SYM_SWITCH("_SymbolExample", "00 00 A0 E3 1E FF 2F E1", boolean);
-            PATCH_LIB_SYM_SWITCH("libNativeGame.so", "_SymbolExample", "00 00 A0 E3 1E FF 2F E1", boolean);
-
-            //Restore patched offset to original
-            RESTORE("0x400000");
-            RESTORE_LIB("libil2cpp.so", "0x400000");
-            RESTORE_SYM("_SymbolExample");
-            RESTORE_LIB_SYM("libil2cpp.so", "_SymbolExample");
-            break;
-        case 110:
-            break;
-        case 1:
-            if (value >= 1) {
-                sliderValue = value;
-            }
-            break;
-        case 2:
-            switch (value) {
-                //For noobies
-                case 0:
-                    RESTORE("0x0");
-                    break;
-                case 1:
-                    PATCH("0x0", "01 00 A0 E3 1E FF 2F E1");
-                    break;
-                case 2:
-                    PATCH("0x0", "02 00 A0 E3 1E FF 2F E1");
-                    break;
-            }
-            break;
-        case 3:
-            switch (value) {
-                case 0:
-                    LOGD(OBFUSCATE("Selected item 1"));
-                    break;
-                case 1:
-                    LOGD(OBFUSCATE("Selected item 2"));
-                    break;
-                case 2:
-                    LOGD(OBFUSCATE("Selected item 3"));
-                    break;
-            }
-            break;
-        case 4:
-            // Since we have instanceBtn as a field, we can call it out of Update hook function
-            if (instanceBtn != NULL)
-                AddMoneyExample(instanceBtn, 999999);
-            // Toast(env, obj, OBFUSCATE("Button pressed"), ToastLength::LENGTH_SHORT);
-            break;
-        case 5:
-            break;
-        case 6:
-            featureHookToggle = boolean;
-            break;
-        case 7:
-            level = value;
-            break;
-        case 8:
-            break;
-        case 9:
             break;
     }
 }
 
 __attribute__((constructor))
 void lib_main() {
-    // Create a new thread so it does not block the main thread, means the game would not freeze
     pthread_t ptid;
     pthread_create(&ptid, NULL, hack_thread, NULL);
 }
